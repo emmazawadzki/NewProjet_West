6ff94e13abafdb929070bea8aeda145449c48529
emmazawadzki <90409741+emmazawadzki@users.noreply.github.com>
2022-10-15 14:52:11 +0200

    Création dépot

--- src/projetwest/ProjetWest.java
+++ src/projetwest/ProjetWest.java
@@ -4,6 +4,8 @@
  */
 package projetwest;
 
+import java.util.Scanner;
+
 /**
  *
  * @author zawae
@@ -14,8 +16,21 @@ public class ProjetWest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Test");
+        Scanner keyboard=new Scanner (System.in);
+        String nom="";
+        String boissonF="";
+        
+        CowBoy[] persoPrincipal=new CowBoy[1];
+    
+        System.out.println("Avant de commencer, quelle est votre nom :");
+        nom=keyboard.nextLine();
+        System.out.println("Et quelle est votre boisson favorite :");
+        boissonF=keyboard.nextLine();
+        
+        persoPrincipal[0]=new CowBoy(nom,boissonF);
+        
+        System.out.println("Vous venez d'entrer dans un saloon");
+        System.out.println("et allez jusqu'au Barman pour commander une boisson.");
     }
     
 }
