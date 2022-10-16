13be4521458ab8eac3eb54766c8ae965b41c62fd
emmazawadzki <90409741+emmazawadzki@users.noreply.github.com>
2022-10-16 16:22:53 +0200

    Création de la date et ajout de 1 jour à chaque partie

--- src/projetwest/ProjetWest.java
+++ src/projetwest/ProjetWest.java
@@ -5,6 +5,9 @@
 package projetwest;
 
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat; 
 
 /**
  *
@@ -16,6 +19,16 @@ public class ProjetWest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Création de la date de départ
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy");
+        String stringDate= format.format(date); 
+        
+        //Monnaie du jeu
+        int solde=10000;
+        
         Scanner keyboard=new Scanner (System.in);
         String nom="";
         String boissonF="";
@@ -29,8 +42,23 @@ public class ProjetWest {
         
         persoPrincipal[0]=new CowBoy(nom,boissonF);
         
-        System.out.println("Vous venez d'entrer dans un saloon");
-        System.out.println("et allez jusqu'au Barman pour commander une boisson.");
+        System.out.println("Début de la partie :");
+        while(solde>=0){
+            //Affichage de la date
+            date=cal.getTime();
+            stringDate= format.format(date); 
+            System.out.println("Aujourd'hui, nous sommes le " + stringDate);
+            
+            //Affichage du salde actuelle
+            System.out.println("Votre solde est de : " + solde);
+        
+            System.out.println("Vous venez d'entrer dans un saloon");
+            System.out.println("et allez jusqu'au Barman pour commander une boisson.");
+            solde-=5000;
+            
+            // Ajout de 1 jour à la date pour la prochaine partie
+            cal.add(Calendar.DATE,1);
+        }
+        System.out.println("Game Over");
     }
-    
-}
+ }
